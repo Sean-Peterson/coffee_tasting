9b1892ea2271e79caf4ec8bb5e67e5a16549958d
sean peterson <seanpeterson11@gmail.com>
2022-07-08 20:46:40 -0500

    wow, kinda doing it! I think I may be able to to this project. Adding a new component here, or at least changing the homepage to have custome stuff like a interface and the use of it in the homepage. Also, there's an uppercase pipe being used here.

--- coffee_tasting/src/app/home/home.component.ts
+++ coffee_tasting/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+
+  public helloWorld: string ="Hello World"
+
+    hero: Hero = {
+      id: 1,
+      name: 'Windstorm'
+    };
 
   ngOnInit(): void {
   }
